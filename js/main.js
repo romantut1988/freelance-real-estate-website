188b6196a953522ccc6c8ae03dfb873fa3581165
romantut1988 <romantut1988@mail.ru>
2022-03-14 09:41:07 +0300

    slider styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/prev.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/next.svg" alt="prev arrow"></button>'
+    });
 });
