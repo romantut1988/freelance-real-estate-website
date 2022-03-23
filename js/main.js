0c01877724236e58a031a63aa5b4ad6c99c3d109
romantut1988 <romantut1988@mail.ru>
2022-03-23 09:49:12 +0300

    slider section styles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(function(){
     $('.slider').slick({
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/prev.svg" alt="next arrow"></button>',
-        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/next.svg" alt="prev arrow"></button>'
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/next.svg" alt="prev arrow"></button>',
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 441,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 });
